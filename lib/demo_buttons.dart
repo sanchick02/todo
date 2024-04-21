d2b65a5347ce59ea5c893c3be48872165f721f68
sanchick02 <sanchick02@users.noreply.github.com>
2024-04-21 16:46:02 +0800

    first commit

---
+++ lib/demo_buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DemoButton extends StatefulWidget {
+  const DemoButton({super.key});
+
+  @override
+  State<DemoButton> createState() {
+    return _DemoButtonState();
+  }
+}
+
+class _DemoButtonState extends State<DemoButton> {
+  var _isUnderstood = false;
+  Widget build(context) {
+    print('UIUpdatesDemo BUILD called');
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = false;
+                });
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = true;
+                });
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        ),
+        if (_isUnderstood) const Text('Awesome!'),
+      ],
+    );
+  }
+}
